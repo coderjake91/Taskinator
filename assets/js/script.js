5c3766d2b580725b6cf69924e37018b5fe688f20
coderjake91 <jacob.b.frazer@gmail.com>
2021-08-01 14:43:56 -0500

    add ability to add task using button

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//select id(s) of DOM elements
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+var createTaskHandler = function(){
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.textContent = "This is a new task";
+        tasksToDoEl.appendChild(listItemEl);
+};
+
+ //add an event listener to buttonEl act on-click and create a new list element with textcontent/style, then append to parent "ul" element
+ buttonEl.addEventListener("click", createTaskHandler);
