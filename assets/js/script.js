687617376fb3f5b18a5ae13f5b4a7810721e7476
coderjake91 <jacob.b.frazer@gmail.com>
2021-08-01 17:47:34 -0500

    introduce event object to prevent defeault browser behavior and modify event listener to handle interaction with the submission form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 //select id(s) of DOM elements
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-//task handler function that creates new list elements and appends them to the unordered-list element
-var createTaskHandler = function(){
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.textContent = "This is a new task";
-        tasksToDoEl.appendChild(listItemEl);
+//task handler function that creates new list elements and appends them to the unordered-list element, accepts event object as am argument
+var createTaskHandler = function(event){
+
+    //prevent browsers default behavior-i.e. refreshing upon form submission
+    event.preventDefault();
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task";
+    tasksToDoEl.appendChild(listItemEl);
 };
 
- //add an event listener to buttonEl act on-click and create a new list element with textcontent/style, then append to parent "ul" element
- buttonEl.addEventListener("click", createTaskHandler);
+ //add an event listener to formEl, act upon submit and create a new list element with text content/style, then append to parent "ul" element
+ formEl.addEventListener("submit", createTaskHandler);
