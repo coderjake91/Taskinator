712a0dbc03c153e1651de6047c5fb465e21002b5
coderjake91 <jacob.b.frazer@gmail.com>
2021-08-01 21:17:47 -0500

    added user input validation for the form fields and in-situ form  resetting functionality upon task submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,14 @@ var taskFormHandler = function(event){
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if(!taskNameInput || !taskTypeInput){
+        alert("Please fill out the entire the task form!");
+        return false;
+    }
+
+    //reset the form input fields using browser-provided DOM element interface reset method for the form element
+    formEl.reset();
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
